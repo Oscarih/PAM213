fefbaec08eb5fd796a85c16e63b661daf3ecd327
Oscarih <124051249@upq.edu.mx>
2025-09-04 15:29:09 -0600

    Creamos la clase y los 4 métodos seleccionados para la Act 2

---
+++ Actividad2/inicioPAM.java
@@ -0,0 +1,68 @@
+public class inicioPAM {
+    public String FechasdeParciales() {
+        return "
+        Fechas de Parciales:
+        * 1er Parcial: 30-09-25
+        * 2do Parcial: 04-10-25
+        * 3er Parcial: 02-12-25";
+    }
+    public String TemarioDeLaAsignatura() {
+        return "
+        Temario De La Asignatura:
+        --------------------------------------------------------------------------------------
+                 UNIDAD DE APRENDIZAJE                        TEORÍA       PRÁCTICA
+        --------------------------------------------------------------------------------------
+        I. Introducción al desarrollo apps moviles              10            8
+        --------------------------------------------------------------------------------------
+        II. Diseño de aplicaciones móviles                      10           14
+        --------------------------------------------------------------------------------------
+        III.Programacion de aplicaciones moviles                12           24
+        --------------------------------------------------------------------------------------
+        IV.Publicacion de aplicaciones moviles                   4            8
+        --------------------------------------------------------------------------------------
+        HORAS POR CUATRIMESTRE                                  36           54
+        --------------------------------------------------------------------------------------
+        HORAS POR SEMANA                                         2            4
+        ";
+    }
+    public String ReglamentoDPOO() {
+        return "
+        Reglamento de POO:
+        1.Se requiere 80% de asistencia para tener derecho a evaluación parcial y 80% de trabajos en clase.
+        2.Se permiten 10 minutos de tolerancia y si el alumno llega después de este tiempo puede permanecer en la clase, pero no se tomará la asistencia (Solamente en los horarios de
+        inicio:7:00a.m y 14:00 p.m).
+        3. Las faltas deberán estar justificadas mediante el correo institucional con un plazo máximo de 24 horas posteriores a la hora de falta en clase mediante correo del tutor (a),
+        justificantes entregados fuera de la fecha límite permitido no se aceptan, únicamente se aceptarán recetas médicas y citatorios jurídicos.(De forma física deben ser presentados al
+        tutor para ser validados y de forma posterior emitidos).
+        4. Las tareas y trabajos deberán subirlas al Classroom de forma individual y no se recibirán de manera extemporánea.(Salvo previo justificante validado por el tutor)
+        5. Las tareas y trabajos presentarlos en tiempo y forma. La demora de un trabajo o tarea sin justificante y/o con justificante fuera del límite no se aceptan.
+        6. Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.
+        7. El plagio o copia de trabajos y/o exámenes, será condicionado a reprobar a la asignatura y se reportará al área correspondiente.
+        8. Cualquier deshonestidad académica será sancionada reprobando el parcial sin derecho a examen final
+        9. En circunstancia de indisciplina o falta de respeto por parte del alumno hacia docentes,administrativos, compañeros o cualquier persona perteneciente a la universidad, se realizará
+        una primera llama de atención, si el alumno hace caso omiso tendrá que abandonar el aula, tres incidencias de este tipo el alumno no tendrá derecho a examen final o parcial.
+        10. Uso de laptops y/o dispositivos móviles quedará limitados a uso exclusivo de las actividades que así lo requieran.
+        11. Prohibido el uso de audífonos durante la clase.
+        12. Prohibido comer y/o tomar líquidos en el salón.
+        13. Prohibido sentarse encima de las mesas , así como columpiarse en las sillas.
+        14.Todo tema académido debe ser revisado primeramente por parte del alumno con el docente, de no resolverse, pasar con su respectivo tutor, y de ser necesario con la coordinación
+        de tutores. En caso de no solucionarse pasar a la dirección del programa educativo (debe mantenerse este seguimiento de forma obligatoria)
+        15.Cualquier situación no prevista en el presente reglamento pasar directamente con la dirección del programa educativo.
+        16. El día destinado a entrega de calificaciones todos los estudiantes deben estar presentes, ese día se entregarán exámenes y se brindará retroalimentación
+        17.Este reglamento entra en vigor después de que se firme o se acepte por la mayoría de los estudiantes asistentes a la primera sesión de la materia, una vez firmado o aceptado por el
+        50% más el jefe de grupo, es vigente para todo alumno inscrito en el curso aunque no esté presente en la primera sesión.";
+    }
+    public String PorsentajesporParcial() {
+        return "
+        Porsentajes por Parcial:
+        --------------------------------------------------------------------------------------------------------------------------------------
+                        EVIDENCIA DE CONOCIMIENTO       EVIDENCIA DE DESEMPEÑO      EVIDENCIA DE PRODUCTO       PROYECTO INTEGRADOR
+        --------------------------------------------------------------------------------------------------------------------------------------
+        * 1er Parcial:             40%                            20%                         30%                       10%
+        --------------------------------------------------------------------------------------------------------------------------------------
+        * 2do Parcial:             40%                            20%                         20%                       20%
+        --------------------------------------------------------------------------------------------------------------------------------------
+        * 3er Parcial:             20%                            10%                         40%                       30%
+
+    }
+}
