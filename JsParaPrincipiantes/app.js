d2dac2a74402fc0ab31010d78b760fd4638e80f6
Oscarih <124051249@upq.edu.mx>
2025-09-10 18:39:04 -0600

    Se aprendieron los tipos de datos

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,1 +1,37 @@
 document.write("<h1>Hello World</h1>");
+// tipos de datos
+"Hello World" // string
+
+'Hello World' // string
+
+1000000 // number
+
+-2.3
+// boolean
+true
+false
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+// object
+'ryan'
+70.4
+14
+true
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+
+}
+{
+    "username": 'Oscar',
+    "score": 80.4,
+    "hours": 24,
+    "proffesional": true
+
+}
+
+console.log("esto es una cadena")
