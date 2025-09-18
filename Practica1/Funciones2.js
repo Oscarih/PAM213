66f87e4d8da630867406858f192eb09ddea5c7ba
Oscarih <124051249@upq.edu.mx>
2025-09-17 20:28:56 -0600

    Create Funciones2.js

---
+++ Practica1/Funciones2.js
@@ -0,0 +1,10 @@
+function cuadrado(numero) {
+  return numero * numero;
+}
+const cuadrado = numero => numero * numero;
+
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(4));
+console.log(cuadrado(8));
+console.log(cuadrado(15));
