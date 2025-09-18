11de80d9b21984d749ecc15d41ab72cc3439eecb
Oscarih <124051249@upq.edu.mx>
2025-09-17 20:34:55 -0600

    Se realizo el tercer ejercicio, creando dos funciones una personalizada

---
+++ Practica1/Funciones3.js
@@ -0,0 +1,6 @@
+const saludoPersonalizado = (nombre, edad) => {
+  return Hola, me llamo ${nombre} y tengo ${edad} años.;
+};
+
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Oscar", 19));
