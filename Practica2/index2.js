dd857f0be75d150f947c2dfa4005648d9d6d4f02
Oscarih <124051249@upq.edu.mx>
2025-09-23 23:33:23 -0600

    Se realiza el segundo ejercicio, usando map y filter

---
+++ Practica2/index2.js
@@ -0,0 +1,9 @@
+const productos = [
+    {nombre: "laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000}
+];
+const filtrados = productos.filter(producto=> producto.precio > 1000);
+const nombres = filtrados.map(producto=> producto.nombre);
+console.log(nombres);
