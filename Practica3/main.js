be3b05646036e218b16c3f572f259bf105525e7a
Oscarih <124051249@upq.edu.mx>
2025-09-25 14:40:32 -0600

    Se corrige y se hacen las pruebas

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,1 +1,14 @@
-import { restar } from "./utils";
+
+import { restar } from './utils.js';
+console.log("--- Pruebas de la función restar ---");
+let resultado1 = restar(15, 5);
+console.log(`15 - 5 = ${resultado1}`);
+let resultado2 = restar(7, 20);
+console.log(`7 - 20 = ${resultado2}`);
+let resultado3 = restar(-10, 3);
+console.log(`-10 - 3 = ${resultado3}`);
+let resultado4 = restar(-8, -8);
+console.log(`-8 - (-8) = ${resultado4}`);
+let resultado5 = restar(50, 0);
+console.log(`50 - 0 = ${resultado5}`);
+console.log("\n--- ¡Todas las pruebas han finalizado! ---");
