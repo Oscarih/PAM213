7c939339e713d443f5eb5dc7a6c910e73c580f1f
Oscarih <124051249@upq.edu.mx>
2025-10-09 14:59:23 -0600

    commit intro

---
+++ intro/App.js
@@ -0,0 +1,10 @@
+
+import ContadorScreen from './screens/ContadorScreen';
+
+// 2. main: zona de componentes, principal 
+export default function App() { 
+  return (
+    
+    <ContadorScreen> </ContadorScreen>
+  );
+}
